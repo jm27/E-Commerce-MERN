cef9dc49ce08b69869dff66fb896b9e436854f00
Jesus Esquer <jme2791@gmail.com>
2020-04-04 14:02:11 -0700

    Category read update and delete working

--- routes/category.js
+++ routes/category.js
@@ -5,11 +5,15 @@ const router = express.Router()
 const {requireSignIn, isAuth, isAdmin} = require("../controllers/auth");
 
 
-const {create, categoryById, read} = require("../controllers/category");
+const {create, categoryById, read, update, remove, list} = require("../controllers/category");
 const {userById} = require("../controllers/user");
 
 router.get('/category/:categoryId', read)
 router.post('/category/create/:userId',requireSignIn, isAuth, isAdmin, create)
+router.put('/category/:categoryId/:userId',requireSignIn, isAuth, isAdmin, update)
+router.delete('/category/:categoryId/:userId',requireSignIn, isAuth, isAdmin, remove)
+router.get('/categories', list)
+
 
 router.param('categoryId', categoryById)
 router.param('userId', userById)
