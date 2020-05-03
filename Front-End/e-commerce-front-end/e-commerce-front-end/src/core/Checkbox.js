81fb11d4085fea6d2c4208dd8ea6fb939a0af760
Jesus Esquer <jme2791@gmail.com>
2020-05-03 14:32:04 -0700

    created shop component added filter by categories

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkbox.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkbox.js
@@ -1,9 +1,26 @@
 import React, {useState, UseEffect} from 'react';
 
 const Checkbox = ({categories}) =>{
+    const [checked, setChecked] = useState([]);
+
+    const handleToggle = (category)=>()=>{
+        const currentCategoryId = checked.indexOf(category) // return first index or -1 
+        const newCheckedCategoryId = [... checked]
+
+        // if currently checked was not already in checked state > push
+        // else pull/take off
+        if(currentCategoryId === -1) {
+            newCheckedCategoryId.push(category)
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1)
+        }
+        console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId)
+    }
+
     return categories.map((c,i)=>(
         <li key={i} className='list-unstyled'>
-            <input type='checkbox' className='form-check-input'></input>
+            <input onChange={handleToggle(c._id)} value={checked.indexOf(c._id===-1)} type='checkbox' className='form-check-input'></input>
             <label className='form-check-label'>{c.name}</label>
         </li>
     ))
