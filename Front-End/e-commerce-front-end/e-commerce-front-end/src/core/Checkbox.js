84962efc16ed24cd815780ced3645ed2b978fde8
Jesus Esquer <jme2791@gmail.com>
2020-05-04 11:41:55 -0700

    filter by price radio button

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkbox.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkbox.js
@@ -1,29 +1,34 @@
-import React, {useState, UseEffect} from 'react';
+import React, { useState, UseEffect } from "react";
 
-const Checkbox = ({categories}) =>{
-    const [checked, setChecked] = useState([]);
+const Checkbox = ({ categories, handleFilters }) => {
+  const [checked, setChecked] = useState([]);
 
-    const handleToggle = (category)=>()=>{
-        const currentCategoryId = checked.indexOf(category) // return first index or -1 
-        const newCheckedCategoryId = [... checked]
+  const handleToggle = (category) => () => {
+    const currentCategoryId = checked.indexOf(category); // return first index or -1
+    const newCheckedCategoryId = [...checked];
 
-        // if currently checked was not already in checked state > push
-        // else pull/take off
-        if(currentCategoryId === -1) {
-            newCheckedCategoryId.push(category)
-        } else {
-            newCheckedCategoryId.splice(currentCategoryId, 1)
-        }
-        console.log(newCheckedCategoryId)
-        setChecked(newCheckedCategoryId)
+    // if currently checked was not already in checked state > push
+    // else pull/take off
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(category);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
     }
-
-    return categories.map((c,i)=>(
-        <li key={i} className='list-unstyled'>
-            <input onChange={handleToggle(c._id)} value={checked.indexOf(c._id===-1)} type='checkbox' className='form-check-input'></input>
-            <label className='form-check-label'>{c.name}</label>
-        </li>
-    ))
-}
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+    handleFilters(newCheckedCategoryId);
+  };
+  return categories.map((c, i) => (
+    <li key={i} className="list-unstyled">
+      <input
+        onChange={handleToggle(c._id)}
+        value={checked.indexOf(c._id === -1)}
+        type="checkbox"
+        className="form-check-input"
+      ></input>
+      <label className="form-check-label">{c.name}</label>
+    </li>
+  ));
+};
 
 export default Checkbox;
--- Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Shop.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Shop.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Card from "./Card";
-import Checkbox from './Checkbox';
+import Checkbox from "./Checkbox";
+import { prices } from "./fixedPrices";
+import RadioBox from "./RadioBox";
 import { getCategories } from "../admin/apiCore";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
@@ -22,6 +27,12 @@ const Shop = () => {
     init();
   }, []);
 
+  const handleFilters = (filters, filterBy) => {
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
+
   return (
     <Layout
       title="Shop Page"
@@ -29,8 +40,23 @@ const Shop = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div className="col-4"><h4>Filter by categories</h4><ul><Checkbox categories={categories}></Checkbox></ul></div>
-        <div className="col-8">right</div>
+        <div className="col-4">
+          <h4>Filter by categories</h4>
+          <ul>
+            <Checkbox
+              categories={categories}
+              handleFilters={(filters) => handleFilters(filters, "category")}
+            ></Checkbox>
+          </ul>
+          <h4>Filter by Price</h4>
+          <div>
+          <RadioBox
+            prices={prices}
+            handleFilters={(filters) => handleFilters(filters, "price")}
+          ></RadioBox>
+          </div>
+        </div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );
