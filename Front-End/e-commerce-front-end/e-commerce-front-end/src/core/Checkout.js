cca2f0084ed984681c90a3e4e35536008e7ebd12
Jesus Esquer <jme2791@gmail.com>
2020-05-12 09:39:16 -0700

    checkout component fixed infinite renders

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkout.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/core/Checkout.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 // import Layout from './Layout';
 // import {getProducts} from './apiCore'
 // import Card from './Card';
+import { isAuthenticated } from "../auth";
+import {Link} from 'react-router-dom'
 
-const Checkout = ({ products, setRun = f => f, run = undefined }) => {
+const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
       return currentValue + nextValue.count * nextValue.price;
@@ -13,6 +15,14 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
   return (
     <div>
       <h2>Total: ${getTotal()}</h2>
+
+      {isAuthenticated() ? (
+        <button className="btn btn-success">Checkout</button>
+      ) : (
+        <Link to="/signin">
+          <button className="btn btn-primary">Sign in to checkout</button>
+        </Link>
+      )}
     </div>
   );
 };
