f1f2c2cb7d4fa9b9cd1960b6f47fe17136a22a95
Jesus Esquer <jme2791@gmail.com>
2020-05-12 13:29:34 -0700

    added method to send payment info to the back end

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/admin/apiCore.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/admin/apiCore.js
@@ -69,3 +69,18 @@ export const listRelated = (productId) => {
     })
     .catch((error) => console.log(error));
 };
+
+export const getBrainTreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(error));
+};
