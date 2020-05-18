986dbc9dcdb36c14b7f2ed84a923e3b648c776dd
Jesus Esquer <jme2791@gmail.com>
2020-05-17 17:03:51 -0700

    update status values

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/admin/apiAdmin.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/admin/apiAdmin.js
@@ -70,3 +70,19 @@ export const getStatusValues = (userId, token)=> {
   .catch(error=>console.log(error))
 }
 
+export const updateOrderStatus = (userId, token, orderId, status)=> {
+  return fetch (`${API}/order/${orderId}/status/${userId}`,{
+    method: 'PUT',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({status, orderId})
+  })
+  .then(response =>{
+    return response.json()
+  })
+  .catch(error=>console.log(error))
+}
+
