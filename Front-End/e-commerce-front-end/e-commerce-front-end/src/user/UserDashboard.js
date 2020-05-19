6e42bbf45ff2524a8e9a025e1e5b7a84da987cca
Jesus Esquer <jme2791@gmail.com>
2020-05-18 17:38:29 -0700

    Get user purchase history

--- Front-End/e-commerce-front-end/e-commerce-front-end/src/user/UserDashboard.js
+++ Front-End/e-commerce-front-end/e-commerce-front-end/src/user/UserDashboard.js
@@ -3,6 +3,7 @@ import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { getPurchaseHistory } from "./apiUser";
+import moment from "moment";
 
 const Dashboard = () => {
   const [history, setHistory] = useState([]);
@@ -67,7 +68,24 @@ const Dashboard = () => {
       <div className="card mb-5">
         <h3 className="card-header">Purchase history</h3>
         <ul className="list-group">
-          <li className="list-group-item">{JSON.stringify(history)}</li>
+          <li className="list-group-item">
+            {history.map((h, i) => {
+              return (
+                <div>
+                  <hr />
+                  {h.products.map((p, i) => {
+                    return (
+                      <div key={i}>
+                        <h6>Product name: {p.name}</h6>
+                        <h6>Product price: ${p.price}</h6>
+                        <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </li>
         </ul>
       </div>
     );
